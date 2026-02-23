a877a33be802ab7baac567272b135bea3f4c715c
Rose Richards <rricha58@skyhawks.utm.edu>
2026-02-23 20:15:03 +0000

    All Pointer Tests

--- pointerTestFolder/pointerTest.cpp
+++ pointerTestFolder/pointerTest.cpp
@@ -16,9 +16,41 @@ using namespace std;
 int main(){
     int testNumA = 12;
     int &testNumB = testNumA;
-    cout << "This should be equal to what was 12: " << testNumB << endl;
+    cout << "This should be equal to twelve: " << testNumB << endl;
     testNumA = 10;
     cout << "This should have updated: " << testNumA << endl;
     cout << "This should be the memory address of testNumA: " << &testNumA << endl
     << "And this should be the memory address of testNumB: " << &testNumB <<endl;
+    //Both testNumA & testNumB should be equal to 10
+
+    int* ptr = &testNumA; //should be equal to &testNumA
+    cout << "This should be the memory address of testNumA without calling testNumA: " 
+    << ptr << endl << "This should be the value of testNumaA without calling it: " << *ptr << endl;
+
+    *ptr = 82;
+    cout << "This should have been updated: " << *ptr << endl 
+    << "This should be that number + 5: " << *ptr + 5 << endl;
+
+    cout << "This should cout the size of testNumA: " << sizeof(testNumA) << endl
+    << "This should cout the size of testNumH: " << sizeof(testNumB) << endl;
+
+    //Alrighty here is the part that scares
+    int* testNumC = new int;
+    *testNumC = 35;
+    cout << "This should cout testNumC: " << *testNumC << endl;
+    delete testNumC;
+    cout << "This shouldn't cout testNumC: " << *testNumC << endl;
+
+    int* testNumD = new int[10];
+    for (int i = 0; i < 10; i++){
+        testNumD[i] = 35 + i;
+    }
+
+    for (int i = 0; i < 10; i++){
+        cout << testNumD[i] << " ";
+    }
+    cout << endl;
+    delete[] testNumD;
+
+
 }
